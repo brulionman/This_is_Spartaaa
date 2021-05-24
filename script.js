832b9de0e927d528e24e07fea66c655c898f0327
brulionman <brulionman@gmail.com>
2021-05-24 18:54:48 +0100

    automatic positioning for boxes done

---
+++ script.js
@@ -0,0 +1,23 @@
+const stripesContainer = document.getElementById('stripes')
+const btn = document.getElementById('btn')
+
+
+
+function createStripes() {
+    for(let s = 0; s < 10; s++) {
+        
+        
+
+        const stripe = document.createElement('div')
+        stripe.classList.add('stripe')
+
+        stripe.style.backgroundPosition = `${-s * 80}px 0`
+
+        stripesContainer.appendChild(stripe)
+        
+        
+        
+    }
+}
+
+createStripes()
